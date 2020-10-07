5f632d1dbed9a5fd5c380e78cfaa58a1f2106bcc
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-10-07 06:48:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,15 +13,16 @@ class Song
     @@all
   end
   
-  def self.new_by_filename(filename)
-    artist_name = filename.split(" - ")[0]
-    song_name = filename.split(" - ")[1]
-    new_song = self.new(song_name)
-  end
-  
   def artist_name(name)
     self.artist = Artist.find_or_create_by_name(name)
     self.artist.add_song(self)
+  end
+  
+  def self.new_by_filename(filename)
+    artistname = filename.split(" - ")[0]
+    song_name = filename.split(" - ")[1]
+    new_song = self.new(song_name)
+    new_song.artist_name(artistname)
   end
   
 end
